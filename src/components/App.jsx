aad99949f009d80aa7028fb647c659b20a74ed33
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-05-21 21:08:30 +0200

    feedback step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      
-    </div>
-  );
+import { Component } from "react";
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(this.state.good / this.countTotalFeedback() * 100)
+  }
+
+  goodFeed = () => {
+    this.setState(state => ({ good: state.good + 1 }));
+  }
+
+  neutralFeed = () => {
+    this.setState(state => ({ neutral: state.neutral + 1 }));
+  }
+
+  badFeed = () => {
+    this.setState(state => ({ bad: state.bad + 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Please leave feedback</h3>
+        <button type="button" onClick={this.goodFeed}>Good</button>
+        <button type="button" onClick={this.neutralFeed}>Neutral</button>
+        <button type="button" onClick={this.badFeed}>Bad</button>
+        <h4>Statistics</h4>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.countTotalFeedback()}</p>
+        <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+      </div>
+    );
+  }
 };
